fc612ef46e06589ce77d35508a936beeed249b18
gaurav <gauravkumar9130@gmail.com>
2021-10-09 16:39:27 +0530

    created linuxvm

---
+++ linuxvm/main.tf
@@ -0,0 +1,82 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.region
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = var.public_ip_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = var.user_name
+  admin_password = "Pa$$w0rd123"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install nginx -y",
+      "sudo systemctl start nginx"
+    ]
+
+    connection {
+      type = "ssh"
+      user = self.admin_username
+      password = self.admin_password
+      host = self.public_ip_address
+    }
+  }
+}
---
+++ linuxvm/var.tf
@@ -0,0 +1,31 @@
+variable "resource_group_name" {
+    default = "rg1"
+}
+
+variable "region" {
+    default = "eastus"
+}
+
+variable "vnet_name" {
+    default = "vnet1"
+}
+
+variable "subnet_name" {
+    default = "subnet1"
+}
+
+variable "public_ip_name" {
+    default = "pip1"
+}
+
+variable "nic_name" {
+    default = "pip1"
+}
+
+variable "vm_name" {
+    default = "vm1"
+}
+
+variable "user_name" {
+    default = "gaurav"
+}
